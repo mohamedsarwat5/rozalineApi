18f0c7fb5a266c8b3c9c3f33c9f6c20d8b027ad7
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-09-15 04:03:24 +0300

    fsda

--- models/Product.js
+++ models/Product.js
@@ -38,7 +38,7 @@ const productSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    SalePrice: {
+    salePrice: {
       type: Number,
       required: false,
     },
